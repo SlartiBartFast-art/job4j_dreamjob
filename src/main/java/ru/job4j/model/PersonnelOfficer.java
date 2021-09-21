682875f36fb280bd7b2c53f3ca2b245dda9b27d1
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-21 15:25:47 +0500

    1. Техническое задание - проект "Работа мечты" [#260362]

---
+++ src/main/java/ru/job4j/model/PersonnelOfficer.java
@@ -0,0 +1,49 @@
+package ru.job4j.model;
+
+public class PersonnelOfficer {
+    private String surname;
+    private String firstName;
+    private String secondName;
+    private int phone;
+    private int id;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
