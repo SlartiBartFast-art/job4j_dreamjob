13eb399f6771dda5d9f3a51b30a549d7a21b298d
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-21 21:05:06 +0500

    2. Scriplet. Динамическая таблица. [#276520]

---
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -0,0 +1,50 @@
+package ru.job4j.dream.model;
+
+import java.util.Objects;
+
+/**
+ * 2. Scriplet. Динамическая таблица. [#276520]
+ * Уровень : 3. Мидл Категория : 3.2.
+ * Servlet JSPТопик : 3.2.2. JSP
+ * path 1
+ * @author SlartiBartFast-art
+ * @since 21.09.21
+ */
+public class Post {
+    private int id;
+    private String name;
+
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -0,0 +1,34 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 2. Scriplet. Динамическая таблица. [#276520]
+ * Уровень : 3. Мидл Категория : 3.2.
+ * Servlet JSPТопик : 3.2.2. JSP
+ * path 2
+ * @author SlartiBartFast-art
+ * @since 21.09.21
+ */
+public class Store {
+    private static final Store INST = new Store();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job"));
+        posts.put(2, new Post(2, "Middle Java Job"));
+        posts.put(3, new Post(3, "Senior Java Job"));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
