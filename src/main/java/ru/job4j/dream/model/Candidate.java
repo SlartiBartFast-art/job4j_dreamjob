e119f30af428f457fddd81b2339e460607f57705
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-22 14:37:44 +0500

    4. candicates.jsp - список кандидатов. [#276522]

---
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -0,0 +1,55 @@
+package ru.job4j.dream.model;
+
+import java.util.Objects;
+
+/**
+ * 4. candicates.jsp - список кандидатов. [#276522]
+ * Уровень : 3. Мидл Категория : 3.2. Servlet JSPТопик : 3.2.2. JSP
+ * модель описывающую кандидата.
+ *
+ * @author SlartiBartFast-art
+ * @since 22.09.21
+ */
+public class Candidate {
+    private int id;
+    private String name;
+
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id
+                && Objects.equals(name, candidate.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
