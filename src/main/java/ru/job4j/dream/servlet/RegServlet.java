ba55b1b097992b6ed313ddff498f88077aed836d
SlartiBartFast-art <JekHamond@tutanota.com>
2021-09-29 14:01:34 +0500

    4. Регистрация пользователя. [#283110 #209712]

--- src/main/java/ru/job4j/dream/servlet/RegServlet.java
+++ src/main/java/ru/job4j/dream/servlet/RegServlet.java
@@ -1,5 +1,8 @@
 package ru.job4j.dream.servlet;
 
+import ru.job4j.dream.model.User;
+import ru.job4j.dream.store.PsqlStore;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,18 +13,31 @@ import java.io.IOException;
  * 4. Регистрация пользователя. [#283110]
  * Уровень : 3. Мидл Категория : 3.2. Servlet JSP Топик : 3.2.6. Filter, Security
  * Создайте страница reg.jsp. Сервлет RegServlet.
+ * Авторизация и регистрацию сделайте через Store.
+ * В предыдущих уроках мы загрузали пользователя в коде.
+ * User admin = new User();
+ * admin.setName("Admin");
+ * admin.setEmail(email);
+ * sc.setAttribute("user", admin);
+ * Нужно это переделать на Store.instOf().findByEmail().
  * @author SlartiBartFast-art
- * @version 01
+ * @version 02
  * @since 28.09.21
  */
 public class RegServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String email = req.getParameter("email");
+        if (PsqlStore.instOf().findByEmail(email) == null) {
+            User user = new User();
+            user.setName(req.getParameter("name"));
+            user.setEmail(email);
+            user.setPassword(req.getParameter("password"));
+            PsqlStore.instOf().save(user);
+            req.setAttribute("error", "Регистрация завершена");
+        } else {
+            req.setAttribute("error", "Пользователь с таким email уже существует");
+        }
+        req.getRequestDispatcher("register/reg.jsp").forward(req, resp);
     }
 }
